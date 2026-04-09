f8676ef23c933f4687547a03b48f951298609ebf
ramanujchaxu-2005 <ramanujchaxu@gmail.com>
2026-04-09 10:06:09 -0400

    Add files via upload

---
+++ UnoActionHandler.java
@@ -0,0 +1,36 @@
+public class UnoActionHandler {
+
+    public static void applyAction(UnoCard card) {
+
+        if (card == null) return;
+
+        String type = card.getType().toLowerCase();
+
+        switch (type) {
+
+            case "skip":
+                System.out.println("Next player skipped!");
+                break;
+
+            case "reverse":
+                System.out.println("Game direction reversed!");
+                break;
+
+            case "draw2":
+                System.out.println("Next player draws 2 cards!");
+                break;
+
+            case "wild":
+                System.out.println("Choose a color!");
+                break;
+
+            case "wilddraw4":
+                System.out.println("Next player draws 4 cards!");
+                break;
+
+            default:
+                // Number card → no action
+                break;
+        }
+    }
+}
---
+++ UnoValidator.java
@@ -0,0 +1,33 @@
+public class UnoValidator {
+
+    public static boolean isValidMove(UnoCard playedCard, UnoCard topCard) {
+
+        if (playedCard == null || topCard == null) {
+            return false;
+        }
+
+        // Wild cards can always be played
+        if (playedCard.getColor().equalsIgnoreCase("Black")) {
+            return true;
+        }
+
+        // Same color
+        if (playedCard.getColor().equalsIgnoreCase(topCard.getColor())) {
+            return true;
+        }
+
+        // Same type (Skip, Reverse, Draw2)
+        if (playedCard.getType().equalsIgnoreCase(topCard.getType())) {
+            return true;
+        }
+
+        // Same number (only for number cards)
+        if (playedCard.getType().equalsIgnoreCase("Number") &&
+            topCard.getType().equalsIgnoreCase("Number") &&
+            playedCard.getNumber() == topCard.getNumber()) {
+            return true;
+        }
+
+        return false;
+    }
+}
